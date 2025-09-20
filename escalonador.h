b077d515545b9f74421f88fa400577caf402a65d
MathDEV-0 <teteuviegas.2006@hotmail.com>
2025-09-20 14:17:24 -0300

    fix

--- escalonador.h
+++ escalonador.h
@@ -1,21 +1,18 @@
-#ifndef SCHEDULER_H
-#define SCHEDULER_H
+#ifndef ESCALONADOR_H
+#define ESCALONADOR_H
 
 #include "pcb.h"
 
 // algoritmos de escalonamento
 typedef enum {
-    ALG_RR, 
     ALG_FIFO,
-    ALG_SJF,
-    ALG_SRT
+    ALG_SJF
 } SchedAlgo;
 
-// funções protótipos
+// Protótipos das funções
+PCB* schedule(SchedAlgo algo, PCB **ready, int quantum);
 PCB* fifo_sch(PCB **ready);
-PCB* sjf_sch(PCB **ready);
-PCB* rr_sch(PCB **ready, int quantum); 
-PCB* srt_sch(PCB **ready);              
+PCB* sjf_sch(PCB **ready);           
 
 
 #endif
